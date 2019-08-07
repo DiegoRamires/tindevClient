7c6564df44c6f6649449379658e578f33168ebb6
DiegoRamires <goncalves.ramires@gmail.com>
2019-08-07 20:23:00 -0300

    styling main page cards

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,100 @@
 import React from 'react'
+import './Main.css'
+
+import logo from '../assets/logo.svg'
+import like from '../assets/like.svg'
+import dislike from '../assets/dislike.svg'
 
 export default function Main({ match }) {
-  return <h1>Dev id nº {match.params.id}</h1>
+  return (
+    <div className="main-container">
+      <img src={logo} alt="Logo"/>
+      <ul>
+        <li>
+          <img src="https://avatars3.githubusercontent.com/u/7052300?v=4" alt=""/>
+          <footer>
+            <strong>Diego Ramires</strong>
+            <p>FrontEnd Developer</p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike"/>
+            </button>
+            <button type="button">
+              <img src={like} alt="Like"/>
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <img src="https://avatars3.githubusercontent.com/u/7052300?v=4" alt=""/>
+          <footer>
+            <strong>Diego Ramires</strong>
+            <p>FrontEnd Developer</p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike"/>
+            </button>
+            <button type="button">
+              <img src={like} alt="Like"/>
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <img src="https://avatars3.githubusercontent.com/u/7052300?v=4" alt=""/>
+          <footer>
+            <strong>Diego Ramires</strong>
+            <p>FrontEnd Developer</p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike"/>
+            </button>
+            <button type="button">
+              <img src={like} alt="Like"/>
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <img src="https://avatars3.githubusercontent.com/u/7052300?v=4" alt=""/>
+          <footer>
+            <strong>Diego Ramires</strong>
+            <p>FrontEnd Developer</p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike"/>
+            </button>
+            <button type="button">
+              <img src={like} alt="Like"/>
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <img src="https://avatars3.githubusercontent.com/u/7052300?v=4" alt=""/>
+          <footer>
+            <strong>Diego Ramires</strong>
+            <p>FrontEnd Developer</p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike"/>
+            </button>
+            <button type="button">
+              <img src={like} alt="Like"/>
+            </button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  )
 }
