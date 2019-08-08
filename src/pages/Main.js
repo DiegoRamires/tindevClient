44af212c03d4dc37cad72da901d5425e56092812
DiegoRamires <goncalves.ramires@gmail.com>
2019-08-07 21:26:46 -0300

    retrieve data from api. use hooks to render devs as a list on main page when user is logged

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,99 +1,50 @@
-import React from 'react'
-import './Main.css'
+import React, { useEffect, useState } from 'react'
+import api from '../services/api'
 
+import './Main.css'
 import logo from '../assets/logo.svg'
 import like from '../assets/like.svg'
 import dislike from '../assets/dislike.svg'
 
 export default function Main({ match }) {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    async function loadUsers() {
+      const response = await api.get('/devs', {
+        headers: {
+          user: match.params.id,
+        }
+      })
+
+      setUsers(response.data)
+    }
+
+    loadUsers()
+  }, [match.params.id])
+
   return (
     <div className="main-container">
       <img src={logo} alt="Logo"/>
       <ul>
-        <li>
-          <img src="https://avatars3.githubusercontent.com/u/7052300?v=4" alt=""/>
-          <footer>
-            <strong>Diego Ramires</strong>
-            <p>FrontEnd Developer</p>
-          </footer>
+        {users.map(user => (
+          <li key={user._id}>
+            <img src={user.avatar} alt={user.name}/>
+            <footer>
+              <strong>{user.name}</strong>
+              <p>{user.bio}</p>
+            </footer>
 
-          <div className="buttons">
-            <button type="button">
-              <img src={dislike} alt="Dislike"/>
-            </button>
-            <button type="button">
-              <img src={like} alt="Like"/>
-            </button>
-          </div>
-        </li>
-
-        <li>
-          <img src="https://avatars3.githubusercontent.com/u/7052300?v=4" alt=""/>
-          <footer>
-            <strong>Diego Ramires</strong>
-            <p>FrontEnd Developer</p>
-          </footer>
-
-          <div className="buttons">
-            <button type="button">
-              <img src={dislike} alt="Dislike"/>
-            </button>
-            <button type="button">
-              <img src={like} alt="Like"/>
-            </button>
-          </div>
-        </li>
-
-        <li>
-          <img src="https://avatars3.githubusercontent.com/u/7052300?v=4" alt=""/>
-          <footer>
-            <strong>Diego Ramires</strong>
-            <p>FrontEnd Developer</p>
-          </footer>
-
-          <div className="buttons">
-            <button type="button">
-              <img src={dislike} alt="Dislike"/>
-            </button>
-            <button type="button">
-              <img src={like} alt="Like"/>
-            </button>
-          </div>
-        </li>
-
-        <li>
-          <img src="https://avatars3.githubusercontent.com/u/7052300?v=4" alt=""/>
-          <footer>
-            <strong>Diego Ramires</strong>
-            <p>FrontEnd Developer</p>
-          </footer>
-
-          <div className="buttons">
-            <button type="button">
-              <img src={dislike} alt="Dislike"/>
-            </button>
-            <button type="button">
-              <img src={like} alt="Like"/>
-            </button>
-          </div>
-        </li>
-
-        <li>
-          <img src="https://avatars3.githubusercontent.com/u/7052300?v=4" alt=""/>
-          <footer>
-            <strong>Diego Ramires</strong>
-            <p>FrontEnd Developer</p>
-          </footer>
-
-          <div className="buttons">
-            <button type="button">
-              <img src={dislike} alt="Dislike"/>
-            </button>
-            <button type="button">
-              <img src={like} alt="Like"/>
-            </button>
-          </div>
-        </li>
+            <div className="buttons">
+              <button type="button">
+                <img src={dislike} alt="Dislike"/>
+              </button>
+              <button type="button">
+                <img src={like} alt="Like"/>
+              </button>
+            </div>
+          </li>
+        ))}
       </ul>
     </div>
   )
